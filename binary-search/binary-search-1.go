d762bb30835af2fc3bbe02c1e158e51faf566937
ruomenger <ruomenger@foxmail.com>
2025-08-28 23:02:16 +0800

    medium: 34. 在排序数组中查找元素的第一个和最后一个位置
    tags: 数组 二分查找

---
+++ binary-search/binary-search-1.go
@@ -0,0 +1,23 @@
+package binary_search
+
+func lowerBound(nums []int, target int) int {
+	left, right := 0, len(nums)-1
+	for left <= right {
+		mid := left + (right-left)>>1
+		if nums[mid] >= target {
+			right = mid - 1
+		} else {
+			left = mid + 1
+		}
+	}
+	return left
+}
+
+func searchRange(nums []int, target int) []int {
+	start := lowerBound(nums, target)
+	if start == len(nums) || nums[start] != target {
+		return []int{-1, -1}
+	}
+	end := lowerBound(nums, target+1) - 1
+	return []int{start, end}
+}
