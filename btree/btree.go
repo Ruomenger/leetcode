220a268961828bd70e15b51914f007037e7263cc
ruomenger <ruomenger@foxmail.com>
2025-08-30 15:26:44 +0800

    simple: 145. 二叉树的后序遍历
    tags: 栈 树 深度优先搜索 二叉树

--- btree/btree.go
+++ btree/btree.go
@@ -50,3 +50,33 @@ func inorderTraversal(root *TreeNode) []int {
 	}
 	return ans
 }
+
+func postorderTraversal(root *TreeNode) []int {
+	ans := []int{}
+	if root == nil {
+		return ans
+	}
+	stack := list.New()
+	node := root
+	var prev *TreeNode
+	for node != nil || stack.Len() != 0 {
+		for node != nil {
+			stack.PushBack(node)
+			node = node.Left
+		}
+		ele := stack.Back()
+		stack.Remove(ele)
+		node = ele.Value.(*TreeNode)
+
+		if node.Right == nil || node.Right == prev {
+			ans = append(ans, node.Val)
+			prev = node
+			node = nil
+		} else {
+			stack.PushBack(node)
+			node = node.Right
+		}
+	}
+
+	return ans
+}
--- btree/btree_test.go
+++ btree/btree_test.go
@@ -50,3 +50,26 @@ func Test_inorderTraversal(t *testing.T) {
 		})
 	}
 }
+
+func Test_postorderTraversal(t *testing.T) {
+	type args struct {
+		root *TreeNode
+	}
+	root1 := &TreeNode{Val: 1}
+	root1.Right = &TreeNode{Val: 2}
+	root1.Right.Left = &TreeNode{Val: 3}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{"test1", args{root1}, []int{3, 2, 1}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := postorderTraversal(tt.args.root); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("postorderTraversal() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
